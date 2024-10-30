1f11f2d738610cd9013630edf823e0bded2307bc
trevor <t.mcinroe@ed.ac.uk>
2024-10-30 20:12:54 +0000

    alpha=0 for cube

--- brax/envs/wrappers/pixels/rendering_utils.py
+++ brax/envs/wrappers/pixels/rendering_utils.py
@@ -452,7 +452,7 @@ def _build_objects(sys: brax.System, pipeline_states: brax.State) -> list[Obj]:
         tex = sys.mj_model.geom_rgba[idx, :3].reshape((1, 1, 3))
         tex = jnp.concatenate([tex, jnp.array(0.0).reshape(1, 1, 1)], -1)
         # reference: https://github.com/erwincoumans/tinyrenderer/blob/89e8adafb35ecf5134e7b17b71b0f825939dc6d9/model.cpp#L215
-        specular_map = jax.lax.full(tex.shape[:2], 2.0)
+        specular_map = jax.lax.full(tex.shape[:3], 2.0)
 
         # Can we use the idx from sys.geom_bodyid to query sys.body_parentid?
         # link_idx = sys.body_parentid[sys.geom_bodyid[idx] - 1]
