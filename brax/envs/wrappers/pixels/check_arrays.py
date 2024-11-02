2dc64ac2db87a74d64fcefb0c8fb7c3e89cc3899
trevor <t.mcinroe@ed.ac.uk>
2024-11-02 11:24:17 +0000

    checking arrays

--- brax/envs/wrappers/pixels/check_arrays.py
+++ brax/envs/wrappers/pixels/check_arrays.py
@@ -8,18 +8,18 @@ keys = ["vertices", "vertex_normals", "faces"]
 numpy_arrays = {}
 jax_arrays = {}
 
-for k in keys:
-    with open(
-        f"/disk/scratch1/tmcinroe/data_assistax/mesh_{mesh_idx}_{k}_numpy.data", "rb"
-    ) as f:
-        data = pickle.load(f)
 
-    print(f"numpy: {data}")
+for mesh_idx in range(57):
+    for k in keys:
+        with open(
+            f"/disk/scratch1/tmcinroe/data_assistax/mesh_{mesh_idx}_{k}_numpy.data",
+            "rb",
+        ) as f:
+            numpy_data = pickle.load(f)
 
-    with open(
-        f"/disk/scratch1/tmcinroe/data_assistax/mesh_{mesh_idx}_{k}_jax.data", "rb"
-    ) as f:
-        data = pickle.load(f)
+        with open(
+            f"/disk/scratch1/tmcinroe/data_assistax/mesh_{mesh_idx}_{k}_jax.data", "rb"
+        ) as f:
+            jax_data = pickle.load(f)
 
-    print(f"jax: {data}")
-    qqq
+        assert np.allclose(numpy_data, jax_data), f"{mesh_idx}: {k}"
