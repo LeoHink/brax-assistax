2788e17a4001842c4a50c96bc5c905177ef62f29
trevor <t.mcinroe@ed.ac.uk>
2024-11-05 10:45:52 +0000

    and so it begins...

--- brax/envs/wrappers/training.py
+++ brax/envs/wrappers/training.py
@@ -290,7 +290,11 @@ class PixelWrapper(PipelineEnv):
 
         print(f"cache:  {self.cached_objects}")
         print(
-            f"test: {self.cached_objects[0].faces_uv.shape} // {type(self.cached_objects[0].faces_uv)}"
+            f"test: {self.cached_objects[0].instance.faces_uv.shape} // {type(self.cached_objects[0].instance.faces_uv)}"
+        )
+
+        print(
+            f"test 2: {self.cached_objects[0].rot}  // {type(self.cached_objects[0].rot)}"
         )
         qqq
         # The VmapWrapper is already handling this. Will likely need to remove
