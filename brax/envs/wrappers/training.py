f9c8a81b34b1ecd9a11f064be384b068c2203a3c
trevor <t.mcinroe@ed.ac.uk>
2024-11-05 10:47:54 +0000

    and so it begins...

--- brax/envs/wrappers/training.py
+++ brax/envs/wrappers/training.py
@@ -290,7 +290,7 @@ class PixelWrapper(PipelineEnv):
 
         print(f"cache:  {self.cached_objects}")
         print(
-            f"test: {self.cached_objects[0].instance.faces_uv.shape} // {type(self.cached_objects[0].instance.faces_uv)}"
+            f"test: {self.cached_objects[0].instance.specular_map.shape} // {type(self.cached_objects[0].specular_map.faces_uv)}"
         )
 
         print(
